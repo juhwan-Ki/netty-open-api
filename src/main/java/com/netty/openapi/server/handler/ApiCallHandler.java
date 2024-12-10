45a8112c6af05b09ee2e1b23b83a4a7629b4f372
juhwan <kjhw990218@gmail.com>
2024-12-10 17:58:17 +0900

    fix: JSON 변환 코드 수정 - JSON으로 두번 변환 하고 있던거 변경

--- src/main/java/com/netty/openapi/server/handler/ApiCallHandler.java
+++ src/main/java/com/netty/openapi/server/handler/ApiCallHandler.java
@@ -3,7 +3,6 @@ package com.netty.openapi.server.handler;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.netty.openapi.common.ApiResponse;
 import com.netty.openapi.dto.RequestDto;
-import com.netty.openapi.dto.ResponseDto;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import org.apache.logging.log4j.LogManager;
@@ -20,15 +19,20 @@ public class ApiCallHandler extends SimpleChannelInboundHandler<RequestDto> {
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, RequestDto req) throws Exception {
-        ctx.writeAndFlush(callOpenApi(req));
+        logger.info("Received request: {}", req);
+        String response = callOpenApi(req);
+        logger.info("Response received: {}", response);
+
+        ctx.writeAndFlush(response);
     }
 
     // http로 tomcat api 호출
-    private ApiResponse<ResponseDto> callOpenApi(RequestDto req) {
+    private String callOpenApi(RequestDto req) throws IOException {
         HttpURLConnection conn = null;
         BufferedWriter writer = null;
         BufferedReader reader = null;
 
+        // TODO : HTTPRequest 객체 사용해보기
         try {
             URL url = new URL("http://localhost:8080" + req.getReqUrl());
             conn = (HttpURLConnection) url.openConnection();
@@ -44,20 +48,22 @@ public class ApiCallHandler extends SimpleChannelInboundHandler<RequestDto> {
             writer.write(json); // JSON 데이터를 전송
             writer.flush();
 
-            int respCode = conn.getResponseCode();
-            if (respCode == HttpURLConnection.HTTP_OK) {
-                reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-
-                return ApiResponse.ok(mapper.readValue(reader, ResponseDto.class));
-            } else {
-                return ApiResponse.error(conn.getResponseMessage());
+            // 결과 받아오기
+            reader = new BufferedReader(new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8));
+            StringBuilder builder = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                builder.append(line);
             }
+            // 결과 리턴
+            return builder.toString();
+
         } catch (Exception e) {
             logger.error("api call failed : (cause : {})", e.getMessage());
-            return ApiResponse.error(e.getMessage());
+            return mapper.writeValueAsString(ApiResponse.error(e.getMessage()));
         } finally {
-            if (writer != null) try { writer.close(); } catch (Exception e) {}
-            if (reader != null) try { reader.close(); } catch (Exception e) {}
+            if (writer != null) try { writer.close(); } catch (Exception ignored) {}
+            if (reader != null) try { reader.close(); } catch (Exception ignored) {}
             if (conn != null) conn.disconnect();
         }
     }
