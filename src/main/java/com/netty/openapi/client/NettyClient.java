9bfe4992dbbc11741d82572b2adc5522c4220080
juhwan <kjhw990218@gmail.com>
2024-12-13 18:00:16 +0900

    feat: 주기적으로 동작하도록 변경

--- src/main/java/com/netty/openapi/client/NettyClient.java
+++ src/main/java/com/netty/openapi/client/NettyClient.java
@@ -5,10 +5,7 @@ import com.netty.openapi.client.handler.ResultHandler;
 import com.netty.openapi.common.Constants;
 import com.netty.openapi.dto.RequestDto;
 import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelFutureListener;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.EventLoopGroup;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
@@ -17,68 +14,117 @@ import io.netty.handler.codec.string.StringEncoder;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 public class NettyClient {
     private static final Logger logger = LogManager.getLogger(NettyClient.class);
-    private EventLoopGroup loopGroup;
+    private final Map<Integer, Integer> reconnectMap = new ConcurrentHashMap<>();
 
     public static void main(String[] args) {
-        NettyClient client = new NettyClient();
-        client.startClient();
+        new NettyClient().startClient();
     }
 
+    // TODO : 모든 채널이 닫히면 클라이언트 종료하는 코드 추가 필요
     public void startClient() {
-        loopGroup = new NioEventLoopGroup();
+        EventLoopGroup loopGroup = new NioEventLoopGroup();
 
-            Bootstrap bootstrap = new Bootstrap();
-            bootstrap.group(loopGroup)
-                    .channel(NioSocketChannel.class)
-                    .handler(new ChannelInitializer<SocketChannel>() {
-                        @Override
-                        protected void initChannel(SocketChannel socketChannel) throws Exception {
-                            socketChannel.pipeline().addLast(new StringEncoder(StandardCharsets.UTF_8), new StringDecoder(StandardCharsets.UTF_8));
-                            socketChannel.pipeline().addLast(new MessageEncoder()); // 서버로 전송할 데이터를 Encoding
-                            socketChannel.pipeline().addLast(new ResultHandler());
-                        }
-                    });
-
-            // TODO : 다른 방법으로 클라이언트를 생성하도록 변경 필요
-            for (int i = 0; i < 10; i++) {
-                ChannelFuture future = bootstrap.connect(Constants.HOST, Constants.HTTP_PORT);
-                int cnt = i;
-                String url;
-                if(cnt / 2 == 0) url = "/api/rentals";
-                else url = "/api/sales";
-                future.addListener( new ChannelFutureListener() {
+        Bootstrap bootstrap = new Bootstrap();
+        bootstrap.group(loopGroup)
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<SocketChannel>() {
                     @Override
-                    public void operationComplete(ChannelFuture channelFuture) throws Exception {
-                        if(channelFuture.isSuccess()) {
-                            RequestDto req = sendRequest(url);
-                            logger.info("{} 번째 Request : {}", cnt, req);
-                            // 메시지 전송
-                            channelFuture.channel().writeAndFlush(req);
-                        }
+                    protected void initChannel(SocketChannel socketChannel) throws Exception {
+                        socketChannel.pipeline().addLast(new StringEncoder(StandardCharsets.UTF_8), new StringDecoder(StandardCharsets.UTF_8));
+                        socketChannel.pipeline().addLast(new MessageEncoder()); // 서버로 전송할 데이터를 Encoding
+                        socketChannel.pipeline().addLast(new ResultHandler());
                     }
-                });
+                })
+                .option(ChannelOption.SO_KEEPALIVE, true); // 연결을 유지하도록
+
+        // n개 클라이언트 생성
+        for (int i = 0; i < 10; i++) {
+            clientConnect(bootstrap, i);
+        }
+    }
+
+    private void clientConnect(Bootstrap bootstrap, int clientId) {
+        bootstrap.connect(Constants.HOST, Constants.TCP_PORT).addListener( new ChannelFutureListener() {
+            @Override
+            public void operationComplete(ChannelFuture channelFuture) throws Exception {
+                // 연결 성공 시 tcp 요청
+                if(channelFuture.isSuccess()) {
+                    tcpConnect(channelFuture.channel(), clientId);
+                } else {
+                    logger.error("connect failed : {}", channelFuture.cause().getMessage());
+                    logger.info("client reconnecting...");
+                    tcpReconnect(bootstrap, clientId);
+                }
             }
-            logger.info("request success");
+        });
     }
 
-    public void stopClient() {
-        loopGroup.shutdownGracefully();
+    // 주기적으로 tcp 요청을 처리
+    private void tcpConnect(Channel channel, int clientId) {
+        // scheduleAtFixedRate(Runnable, initialDelay, period, time) -> 일정 시간마다 주기적으로 요청하는 스케줄러
+        ScheduledFuture<?> scheduledTask = channel.eventLoop().scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    String url = clientId / 2 == 0 ? Constants.API_RENTAL_URL : Constants.API_SALES_URL;
+                    RequestDto req = sendRequest(url);
+                    logger.info("Request : {}",  req);
+                    // 메시지 전송
+                    channel.writeAndFlush(req);
+                } catch (Exception e) {
+                    logger.error("client id {} request failed : {} ", clientId, e.getMessage());
+                }
+            }
+        }, 0, 1, TimeUnit.MINUTES);
+
+        channel.closeFuture().addListener(new ChannelFutureListener() {
+            @Override
+            public void operationComplete(ChannelFuture future) throws Exception {
+                logger.info("client not active : clientId={}", clientId);
+                scheduledTask.cancel(false);
+            }
+        });
+    }
+    // 서버에서 오류나면 다시 연결
+    private void tcpReconnect(Bootstrap bootstrap, int clientId) {
+        if(!reconnectMap.containsKey(clientId)) {
+            reconnectMap.put(clientId, 1);
+        }
+        int reconnectCnt = reconnectMap.get(clientId);
+        // 3회 이상 이면 연결 끊어버림
+        if(reconnectCnt > 3) {
+            logger.info("client ID : {} closed", clientId);
+            reconnectMap.remove(clientId);
+            return;
+        }
+        logger.info("client ID : {} reconnect cnt : {}", clientId, reconnectCnt);
+        // 30초 후 재연결
+        reconnectMap.put(clientId, reconnectCnt + 1);
+        bootstrap.config().group().schedule(new Runnable() {
+            @Override
+            public void run() {
+                clientConnect(bootstrap, clientId);
+            }
+        }, 30, TimeUnit.SECONDS);
     }
 
-    private RequestDto sendRequest(String url) throws IOException {
-        int ranNum = (int) ( Math.random() * 3 + 1);
+    private RequestDto sendRequest(String url) {
+        int ranNum = (int) (Math.random() * 3 + 1);
         // 1번 일때는 healthCheck 2번은 apiCall 3번 close
-        logger.info("ranNum: {}", ranNum);
+        String num = String.valueOf(ranNum);
         if(ranNum == 1 || ranNum == 3) {
-            return new RequestDto(String.valueOf(ranNum));
+            return new RequestDto(num);
         }
         else if(ranNum == 2) {
-            return new RequestDto(String.valueOf(ranNum), url);
+            return new RequestDto(num, url, num);
         }
         else {
             throw new IllegalArgumentException("Invalid ranNum");
